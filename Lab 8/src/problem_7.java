d819e0c984693d1de180599ef0ab7519c7bfb701
maxbo <maxbo@LAPTOP-GLJ28FNK>
2018-10-10 22:46:38 -0400

    h

---
+++ Lab 8/src/problem_7.java
@@ -0,0 +1,26 @@
+
+public class problem_7 {
+	public static void main(String[] args) {
+		String a = "ABCDEF"; 
+		
+		Hello(a);
+		Systeabc(a);
+		}
+	public static void Hello(String a) {
+		for (int i = 0; i< a.length(); i=i+2) {
+			char H = a.charAt(i);
+			
+			System.out.println(H);
+			
+	}
+		
+		}
+	public static void abc(String a) {
+		for (int i = 0; i< a.length(); i=i+1) {
+			char I = a.charAt(i);
+			
+			System.out.println(I);
+
+	}
+}
+}
