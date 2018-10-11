934e72780bb9b0f564346a807cdc9ac0d3dd0415
mboriboune <boribounemax@gmail.com>
2018-10-10 23:03:46 -0400

    update on problem 7

--- Lab 8/src/problem_7.java
+++ Lab 8/src/problem_7.java
@@ -2,25 +2,13 @@
 public class problem_7 {
 	public static void main(String[] args) {
 		String a = "ABCDEF"; 
-		
 		Hello(a);
-		Systeabc(a);
 		}
-	public static void Hello(String a) {
-		for (int i = 0; i< a.length(); i=i+2) {
-			char H = a.charAt(i);
-			
-			System.out.println(H);
-			
-	}
-		
+	public static void Hello(String b) {
+		for (int i = 0; i< b.length(); i++) {
+			System.out.print(b.charAt(i));
+			i++;
+			System.out.println(b.charAt(i));				
 		}
-	public static void abc(String a) {
-		for (int i = 0; i< a.length(); i=i+1) {
-			char I = a.charAt(i);
-			
-			System.out.println(I);
-
 	}
-}
 }
